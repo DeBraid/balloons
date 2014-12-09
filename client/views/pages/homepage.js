26a452f8981a5b3da2e56a300f894ae981a6bfeb
DeBraid <de.braid@gmail.com>
2014-12-09 18:03:17 -0500

    moved assets for, and fixed homepage

--- client/views/pages/homepage.js
+++ client/views/pages/homepage.js
@@ -1,7 +1,7 @@
 Template.carousel.helpers({
   carouselItems: function () {
     return [
-      { src : "catalogue-cover.png" , path : "promoflags" , klass : "item active" },
+      { src : "cat-cover.png" , path : "promoflags" , klass : "item active" },
       { src : "quicklink-carousel.png" , path : "quicklinkonecolour" , klass : "item" },
       { src : "event-tents-carousel.jpg" , path : "eventtents" , klass : "item" },
       { src : "play-balls-carousel.jpg" , path : "admaxminisportballs" , klass : "item" }      
