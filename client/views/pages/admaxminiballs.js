cb87d4b5bfd107a6aebb2e59ee3dd6c81257c36f
DeBraid <de.braid@gmail.com>
2014-12-29 16:20:27 -0500

    added admax sport balls pals

--- client/views/pages/admaxminiballs.js
+++ client/views/pages/admaxminiballs.js
@@ -38,3 +38,65 @@ Template.admaxminisportballs.helpers({
         };
     }
 });
+
+Template.admaxPalsAll.helpers({
+  admaxPalettes: function () {
+    return [
+      { title : "Basketball Colours - Couleurs de Basketball", src : 'balls/basketballs/basketball-colours.png' , myTemplate : "admaxBasketball" , data : "basicpals"  },
+      { title : "Football Colours - Couleurs de Football", src : 'balls/footballs/solid/football-colours.png' , myTemplate : "admaxFootball" , data : "basicpals"  },
+      { title : "Stock Ink and Stripe Colours - Couleurs d’encre et de rayures", src : 'balls/footballs/striped/stripe-colors.png' , myTemplate : "admaxStock" , data : "basicpals"  },
+      { title : "Play Ball Colours - Couleurs de ballons", src : 'balls/play-balls/playball-colours.png' , myTemplate : "admaxPlay" , data : "crystalpals" },
+      { title : "Baseball Colours - Couleurs de Baseball", src : 'balls/baseballs/baseball.png' , myTemplate : "admaxBaseball" , data : "funpals" },
+      { title : "Soccer Ball Colours - Couleurs de soccer", src : 'balls/soccer/soccer-ball.png' , myTemplate : "admaxSoccer" , data : "soccerpals" }
+    ]
+  }
+});
+
+Template.admaxBasketball.helpers({
+    pals: function () {
+        return [
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/blue.png"},
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/green.png"},
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/orange.png"},
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/red.png"},
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/white.png"},
+            { id : "basketballs", klass : "pal-image indiv", src : "balls/basketballs/yellow.png"},
+        ]
+    }
+});
+
+Template.admaxFootball.helpers({
+    pals: function () {
+        return [
+            { id : "footballs", klass : "pal-image indiv", src : ""},
+        ]
+    }
+});
+Template.admaxStock.helpers({
+    pals: function () {
+        return [
+            { id : "", klass : "", src : ""},
+        ]
+    }
+});
+Template.admaxPlay.helpers({
+    pals: function () {
+        return [
+            { id : "", klass : "", src : ""},
+        ]
+    }
+});
+Template.admaxBaseball.helpers({
+    pals: function () {
+        return [
+            { id : "", klass : "", src : ""},
+        ]
+    }
+});
+Template.admaxSoccer.helpers({
+    pals: function () {
+        return [
+            { id : "", klass : "", src : ""},
+        ]
+    }
+});
