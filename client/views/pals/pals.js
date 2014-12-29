7110dae9833f430226223242b1f0a4f3b5f9b88b
DeBraid <de.braid@gmail.com>
2014-12-29 15:51:10 -0500

    removed references to pills from pals

--- client/views/pals/pals.js
+++ client/views/pals/pals.js
@@ -55,62 +55,6 @@ Template.adritePalsAll.helpers({
   }
 });
 
-Template.pills.helpers({
-  palType: function  (argument) {
-    var data = {tmplName: "basicpals", tmplName: "crystalpals", tmplName: "funpals", tmplName: "metallicpals"};
-    return Template['pills'](data);
-  },
-  basicpals: function () {
-    return [
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Blue.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Green.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Orange.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Pink.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Red.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/White.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/Yellow.png' },
-      { id : 'basic' , klass : 'pal-image indiv' , src : 'adrite/basic/light-blue.png' }
-    ]
-  },
-  crystalpals: function () {
-    return [
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Black.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Blue.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Brown.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Burgundy.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Green.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Magenta.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Purple.pxr' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Red.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Turquoise.png' },
-      { id : 'crystal' , klass : 'pal-image indiv' , src : 'adrite/crystal/Yellow.png' }
-    ]
-  },
-  funpals: function () {
-    return [
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/Butterscotch.png' },
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/Lilac.png' },
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/dark-pink.png' },
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/island-blue.png' },
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/kiwi-lime.png' },
-      { id: 'fun' , klass : 'pal-image indiv' , src : 'adrite/fun/teal.png' }
-    ]
-  },
-  metallicpals: function () {
-   return [
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Blue.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Gold.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Pink.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Purple.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Raspberry.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Red.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/Silver.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/White.png' },
-      { id : 'metallic' , klass : 'pal-image indiv' , src : 'adrite/metallic/dark-blue.png' }
-    ] 
-  }
-});
-
 Template.adritebasic.helpers({
   pals: function () {
     return [
