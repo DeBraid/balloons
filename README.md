043510f908a202b59fe197dc0441bd1cabd5db63
DeBraid <de.braid@gmail.com>
2015-01-13 14:20:48 -0500

    event and resources menu french content added

--- README.md
+++ README.md
@@ -8,14 +8,13 @@ Transform the paper-based product catalogue into digital version.  Uses [Meteor]
 
 
 ### TO DO JAN 13
-1. Products by Market -- products.php (see dot com)
+2. French Language Content (pouce for inches (_fr) )
+2. written description of colour assortments can be removed
 2. No colour palette for SuperAgate (multi colour)
 2. Create page for Stock Balloons
-2. French Language Content
-2. pouce for inches (_fr)
 2. for MENU: add fr on same line (see heading translations provided)
-2. written description of colour assortments can be removed
 2. change all font to black
+1. Products by Market -- products.php (see dot com)
 
 -----------------
 #### Dec 23rd
--- client/views/common/nav/resources.js
+++ client/views/common/nav/resources.js
@@ -1,13 +1,13 @@
 Template.resourcesMenu.helpers({
   resources: function () {
     return [
-      { title : "QuickLink Instructions" , href : "http://www.pioneerline.com/downloads/instructions/QL_Instructions.pdf" },
-      { title : "Proper Balloon Inflation" , href : "http://www.pioneerline.com/downloads/13fliers/Proper_Balloon_Inflation_Instructions.pdf" },
-      { title : "AdPunch Instructions" , href : "http://www.pioneerline.com/downloads/13fliers/AdPunch_Ball_Inflation_Instructions.pdf" },
-      { title : "Inflate Vinyl Balloons" , href : "http://www.pioneerline.com/downloads/instructions/inflate_vinyl.pdf" },
-      { title : "Qualatex Latex Templates" , href : "http://www.pioneerline.com/latex_templates.php" },
-      { title : "Adrite Latex Templates" , href : "http://www.pioneerline.com/economy_templates.php " },
-      { title : "Microfoil Balloons Templates" , href : "http://www.pioneerline.com/microfoil_templates.php " }
+      { title_fr : "Instructions QuickLink" , title : "QuickLink Instructions" , href : "http://www.pioneerline.com/downloads/instructions/QL_Instructions.pdf" },
+      { title_fr : "Balloon inflation appropriée" , title : "Proper Balloon Inflation" , href : "http://www.pioneerline.com/downloads/13fliers/Proper_Balloon_Inflation_Instructions.pdf" },
+      { title_fr : "AdPunch Instructions " , title : "AdPunch Instructions" , href : "http://www.pioneerline.com/downloads/13fliers/AdPunch_Ball_Inflation_Instructions.pdf" },
+      { title_fr : "Gonfler des ballons vinyle " , title : "Inflate Vinyl Balloons" , href : "http://www.pioneerline.com/downloads/instructions/inflate_vinyl.pdf" },
+      { title_fr : "Qualatex Latex Templates" , title : "Qualatex Latex Templates" , href : "http://www.pioneerline.com/latex_templates.php" },
+      { title_fr : "Adrite Latex Templates" , title : "Adrite Latex Templates" , href : "http://www.pioneerline.com/economy_templates.php " },
+      { title_fr : "Ballons microfoil Modèles" , title : "Microfoil Balloons Templates" , href : "http://www.pioneerline.com/microfoil_templates.php " }
     ] 
   }
 });
